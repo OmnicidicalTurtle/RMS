b4c3c330e8f739b444da0bc0484ebf7ac251885a
Sagar Parajuli <parajuli.sagar759@gmail.com>
2024-06-29 15:58:18 +0545

    initial test commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_RMS_FILE D:/ProjectQt/RMS/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/RMS.exe)
+set(__QT_DEPLOY_TARGET_RMS_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_RMS_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
---
+++ mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // Set column count and headers
+    ui->tableWidget->setColumnCount(3);
+    QStringList headers;
+    headers << "Column 1" << "Column 2" << "Column 3";
+    ui->tableWidget->setHorizontalHeaderLabels(headers);
+
+    // Add rows and data
+    ui->tableWidget->setRowCount(3);
+    ui->tableWidget->setItem(0, 0, new QTableWidgetItem("Item 0,0"));
+    ui->tableWidget->setItem(0, 1, new QTableWidgetItem("Item 0,1"));
+    ui->tableWidget->setItem(0, 2, new QTableWidgetItem("Item 0,2"));
+
+    ui->tableWidget->setItem(1, 0, new QTableWidgetItem("Item 1,0"));
+    ui->tableWidget->setItem(1, 1, new QTableWidgetItem("Item 1,1"));
+    ui->tableWidget->setItem(1, 2, new QTableWidgetItem("Item 1,2"));
+
+    ui->tableWidget->setItem(2, 0, new QTableWidgetItem("Item 2,0"));
+    ui->tableWidget->setItem(2, 1, new QTableWidgetItem("Item 2,1"));
+    ui->tableWidget->setItem(2, 2, new QTableWidgetItem("Item 2,2"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
